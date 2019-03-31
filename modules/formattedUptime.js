1b7d1838ca8765dae65bc9adaf9dc101d75d6057
Agnieszka Matyja <agmatyja@gmail.com>
2019-03-31 23:26:30 +0200

    initial commit

---
+++ modules/formattedUptime.js
@@ -0,0 +1,12 @@
+var uptime = require('os');
+
+function formattedUptime() {
+	var time = os.uptime();
+	var hours = Math.floor(time / 3600);
+	var minutes = Math.floor((time - hours * 3600) / 60);
+	var seconds = time - (hours * 3600) - (minutes * 60); 
+	
+	return hours + 'h ' + minutes + 'min ' + seconds + 'sec.';
+}	
+
+exports.print = getformattedUptime;
